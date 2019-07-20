267cad82528f583cbf41d72ddad4e82c01786d6b
Rick Cavalla <monolith32@gmail.com>
2019-07-20 06:45:29 -0400

    DROP sql and ERD image

--- Pewlett_Hackard_Table_Drop.sql
+++ Pewlett_Hackard_Table_Drop.sql
@@ -1,3 +1,6 @@
+-- Not part of project, just to make my life easier
+-- when I goof up and need to start over
+
 DROP TABLE IF EXISTS dept_emp;
 DROP TABLE IF EXISTS dept_manager;
 DROP TABLE IF EXISTS salaries;
